e00e6259d118b2021f80841391ca15ab797bb199
Seung Ho Jang <shjang1992@gmail.com>
2023-12-07 14:28:48 +0900

    Test code commit

--- src/src/main.rs
+++ src/src/main.rs
@@ -1,3 +1,14 @@
+use candle_core::{Device, Tensor};
+use candle_nn::Dropout;
+
 fn main() {
     println!("Hello, world!");
+
+    let device = Device::Cpu;
+
+    let tensor = Tensor::new(&[2u32, 2, 2], &device).unwrap();
+    let dropout = Dropout::new(0.5);
+    let result = dropout.forward(&tensor, true);
+
+    println!("{:?}", result.unwrap());
 }
