35e5fd4342dbfa427b612d03ca8db34e93a0c82c
Seung Ho Jang <shjang1992@gmail.com>
2023-12-08 20:03:58 +0900

    Tensor loading added

--- src/src/component/model.rs
+++ src/src/component/model.rs
@@ -1,4 +1,8 @@
+use std::fs::File;
+
 use candle_core::Device;
+use memmap2::MmapOptions;
+use safetensors::SafeTensors;
 
 pub struct ModelLoader {}
 
@@ -7,6 +11,14 @@ impl ModelLoader {
         println!("Loading model...");
 
         let model_dir = std::path::Path::new(model_dir);
+
+        let filename = model_dir.join("model.safetensors");
+        let file = File::open(filename).unwrap();
+        let buffer = unsafe { MmapOptions::new().map(&file).unwrap() };
+        let tensors = SafeTensors::deserialize(&buffer).unwrap();
+        println!("{:?}", tensors.names());
+
+        // let tensor = tensors.tensor("test").unwrap();
 
         println!("Loading model done")
     }
