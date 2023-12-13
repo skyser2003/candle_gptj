108e25fc90258a373e6fd3e363ba10c8bbac7e48
Seung Ho Jang <shjang1992@gmail.com>
2023-12-13 18:23:49 +0900

    Device parameter added

--- src/src/main.rs
+++ src/src/main.rs
@@ -1,8 +1,9 @@
 mod component;
 
+use anyhow::anyhow;
 use clap::Parser;
 
-use candle_core::{Device, Tensor};
+use candle_core::{backend::BackendDevice, CudaDevice, Device, Tensor};
 use candle_nn::Dropout;
 
 use component::model::ModelLoader;
@@ -14,6 +15,9 @@ struct Arguments {
 
     #[arg(short, long)]
     tokenizer_dir: String,
+
+    #[arg(short, long)]
+    device: Option<String>,
 }
 
 #[tokio::main]
@@ -24,8 +28,30 @@ async fn main() {
 
     let model_dir = args.model_dir;
     let tokenizer_dir = args.tokenizer_dir;
+    let device_str = args.device.clone();
 
-    let device = Device::Cpu;
+    let device_result = || -> anyhow::Result<Device> {
+        let device_str = device_str.unwrap_or("cpu".to_string());
+        let cuda_id = device_str.split("cuda:").collect::<Vec<_>>();
+
+        if cuda_id.len() != 2 {
+            return Err(anyhow!(
+                "Invalid cuda device format, should be in form of 'cuda:<integer>'"
+            ));
+        }
+
+        let cuda_id = cuda_id[1].parse::<usize>()?;
+
+        Ok(Device::Cuda(CudaDevice::new(cuda_id)?))
+    }();
+
+    let device = device_result.unwrap_or(Device::Cpu);
+
+    if device.is_cpu() {
+        println!("Using device 'cpu'");
+    } else if device.is_cuda() {
+        println!("Using device '{}'", args.device.unwrap());
+    }
 
     let loader = ModelLoader::new(&model_dir, &tokenizer_dir, &device);
 
