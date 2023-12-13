e71a641a9f96674f5a61a513991143b3b6f2ea6e
Seung Ho Jang <shjang1992@gmail.com>
2023-12-14 01:10:55 +0900

    Test code moved

--- src/src/main.rs
+++ src/src/main.rs
@@ -1,10 +1,10 @@
 mod component;
+mod test;
 
 use anyhow::anyhow;
 use clap::Parser;
 
-use candle_core::{backend::BackendDevice, CudaDevice, Device, Tensor};
-use candle_nn::Dropout;
+use candle_core::{backend::BackendDevice, CudaDevice, Device};
 
 use component::model::ModelLoader;
 
@@ -60,16 +60,4 @@ async fn main() {
         "Vocab size: {:?}",
         loader.get_tokenizer().get_vocab_size(true)
     );
-
-    // Test
-    let tensor1 = Tensor::new(&[[1u32, 2, 3]], &device).unwrap();
-    let tensor2 = Tensor::new(&[[3u32], [2], [1]], &device).unwrap();
-
-    let result = tensor1.broadcast_mul(&tensor2);
-    println!("{}", result.unwrap());
-
-    let dropout = Dropout::new(0.5);
-    let result = dropout.forward(&tensor1, true);
-
-    println!("{:?}", result.unwrap());
 }
---
+++ src/src/test/tensor.rs
@@ -0,0 +1,25 @@
+use candle_core::{Device, Tensor};
+use candle_nn::{self, Dropout};
+use tokio;
+
+#[tokio::test]
+async fn broadcast_mul() {
+    let device = Device::Cpu;
+
+    let tensor1 = Tensor::new(&[[1u32, 2, 3]], &device).unwrap();
+    let tensor2 = Tensor::new(&[[3u32], [2], [1]], &device).unwrap();
+
+    let result = tensor1.broadcast_mul(&tensor2);
+    println!("{}", result.unwrap());
+}
+
+#[tokio::test]
+async fn dropout() {
+    let device = Device::Cpu;
+
+    let tensor1 = Tensor::new(&[[1u32, 2, 3]], &device).unwrap();
+    let dropout = Dropout::new(0.5);
+    let result = dropout.forward(&tensor1, true);
+
+    println!("{:?}", result.unwrap());
+}
