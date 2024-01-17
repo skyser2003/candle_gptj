85ec1b377d702fb4d054155d375016637f68516d
Seung Ho Jang <shjang1992@gmail.com>
2024-01-18 00:08:52 +0900

    Framework argument added

--- src/src/main.rs
+++ src/src/main.rs
@@ -4,10 +4,10 @@ mod test;
 use anyhow::anyhow;
 use clap::Parser;
 
-use candle_core::{backend::BackendDevice, CudaDevice, Device};
+use candle_core::{backend::BackendDevice, CudaDevice};
 
-use component::model_candle::ModelLoader;
-use component::model_tch::ModelLoader as ModelLoader2;
+use component::model_candle;
+use component::model_tch;
 use tokio::time::Instant;
 
 #[derive(Parser, Debug)]
@@ -20,6 +20,36 @@ struct Arguments {
 
     #[arg(short, long)]
     device: Option<String>,
+
+    #[arg(short, long)]
+    framework: Option<String>,
+}
+
+enum FrameWorkType {
+    Candle,
+    Torch,
+}
+
+#[derive(PartialEq)]
+enum DeviceType {
+    Cpu,
+    Cuda(usize),
+}
+
+fn get_candle_device(device: DeviceType) -> candle_core::Device {
+    match device {
+        DeviceType::Cpu => candle_core::Device::Cpu,
+        DeviceType::Cuda(cuda_id) => {
+            candle_core::Device::Cuda(candle_core::CudaDevice::new(cuda_id).unwrap())
+        }
+    }
+}
+
+fn get_tch_device(device: DeviceType) -> tch::Device {
+    match device {
+        DeviceType::Cpu => tch::Device::Cpu,
+        DeviceType::Cuda(cuda_id) => tch::Device::Cuda(cuda_id),
+    }
 }
 
 #[tokio::main]
@@ -31,8 +61,9 @@ async fn main() -> anyhow::Result<()> {
     let model_dir = args.model_dir;
     let tokenizer_dir = args.tokenizer_dir;
     let device_str = args.device.clone();
+    let framework = args.framework.clone();
 
-    let device_result = || -> anyhow::Result<Device> {
+    let device_result = || -> anyhow::Result<DeviceType> {
         let device_str = device_str.unwrap_or("cpu".to_string());
         let cuda_id = device_str.split("cuda:").collect::<Vec<_>>();
 
@@ -44,33 +75,53 @@ async fn main() -> anyhow::Result<()> {
 
         let cuda_id = cuda_id[1].parse::<usize>()?;
 
-        Ok(Device::Cuda(CudaDevice::new(cuda_id)?))
+        Ok(DeviceType::Cuda(cuda_id))
     }();
 
-    let device = device_result.unwrap_or(Device::Cpu);
+    let device_type = device_result.unwrap_or(DeviceType::Cpu);
 
-    if device.is_cpu() {
+    if device_type == DeviceType::Cpu {
         println!("Using device 'cpu'");
-    } else if device.is_cuda() {
+    } else {
         println!("Using device '{}'", args.device.unwrap());
     }
     println!("");
 
-    // let mut loader = ModelLoader::new(&model_dir, &tokenizer_dir, &device);
-    let mut loader2 = ModelLoader2::new(&model_dir, &tokenizer_dir, &tch::Device::Cpu);
+    let framework = if framework == Some("candle".to_string()) {
+        FrameWorkType::Candle
+    } else {
+        FrameWorkType::Torch
+    };
+
     let inputs = ["Hello who are you?", "What is your name?"];
 
-    let start_time = Instant::now();
-    // let outputs = loader.inference(&inputs)?;
-    let outputs = loader2.inference(&inputs)?;
-    let end_time = Instant::now();
+    let (outputs, elapsed) = match framework {
+        FrameWorkType::Candle => {
+            let device = get_candle_device(device_type);
+            let mut loader = model_candle::ModelLoader::new(&model_dir, &tokenizer_dir, &device);
+
+            let start_time = Instant::now();
+            let outputs = loader.inference(&inputs)?;
+            let end_time = Instant::now();
+
+            (outputs, end_time - start_time)
+        }
+        FrameWorkType::Torch => {
+            let device = get_tch_device(device_type);
+            let mut loader = model_tch::ModelLoader::new(&model_dir, &tokenizer_dir, &device);
+
+            let start_time = Instant::now();
+            let outputs = loader.inference(&inputs)?;
+            let end_time = Instant::now();
+
+            (outputs, end_time - start_time)
+        }
+        _ => unreachable!(),
+    };
 
     println!("Inputs: {:?}", inputs);
     println!("Outputs: {:?}, length: {}", outputs, outputs.len());
-    println!(
-        "Total single token time: {}",
-        (end_time - start_time).as_secs_f32()
-    );
+    println!("Total single token time: {}", elapsed.as_secs_f32());
 
     Ok(())
 }
