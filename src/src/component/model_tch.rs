f9679835cac1110c2f9c2473716748d602ab960e
Seung Ho Jang <shjang1992@gmail.com>
2024-01-23 02:29:31 +0000

    Revert "Remove unncessary contiguous"
    This reverts commit d5c16e8e94e066f2026a5d3cd0c1e9c61f4ddefd.

--- src/src/component/model_tch.rs
+++ src/src/component/model_tch.rs
@@ -902,10 +902,10 @@ impl Attention {
 
         let embed_positions = self.get_embed_positions(position_ids);
 
-        let repeated_position_ids =
-            position_ids
-                .unsqueeze(-1)
-                .repeat(&[1, 1, *embed_positions.size().last().unwrap()]);
+        let repeated_position_ids = position_ids
+            .unsqueeze(-1)
+            .repeat(&[1, 1, *embed_positions.size().last().unwrap()])
+            .contiguous();
 
         let sincos = embed_positions.gather(1, &repeated_position_ids, false);
         let sincos_half_count = sincos.size().last().unwrap() / 2;
